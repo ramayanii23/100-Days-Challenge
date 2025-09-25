9bfb2f6ead22bf1aa38baf7b53f8c2fe120c508c
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2025-09-25 18:01:32 +0530

    Add files via upload

---
+++ uppercaseQ15.c
@@ -0,0 +1,19 @@
+/*Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+*/# include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character");
+    scanf("%c",&ch);
+    if( ch>='A' && ch<='Z')
+    printf("It is uppercase alpahbet");
+    else if(ch>='a' && ch<='z')
+    printf("It is a lowercase");
+    else if (ch>='0'&& ch<='9')
+    printf("It is a digit");
+    else
+    printf("It is a special character");
+    return 0;
+
+
+}
