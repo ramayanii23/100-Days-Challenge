b0843cee20adea3e2869e4d2329a3f49ed9cf4eb
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2025-11-24 23:22:17 +0530

    Add files via upload

---
+++ q87.c
@@ -0,0 +1,39 @@
+/*Count spaces, digits, and special characters in a string.
+*/
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);   
+
+    for (int i = 0; str[i] != '\0'; i++) 
+    {
+
+        if (str[i] == ' ') 
+        {
+            spaces++;
+        }
+        else if (str[i] >= '0' && str[i] <= '9') 
+        {
+            digits++;
+        }
+        else if ((str[i] >= 'A' && str[i] <= 'Z') ||
+                 (str[i] >= 'a' && str[i] <= 'z')) 
+                 {
+            
+        }
+        else {
+            special++;   
+        }
+    }
+
+    printf("Spaces: %d\n", spaces);
+    printf("Digits: %d\n", digits);
+    printf("Special characters: %d\n", special);
+
+    return 0;
+}
