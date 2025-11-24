107a94632f20c2e421687556591c8862f3b15e37
ramayanii23 <Ramayani.28109@stu.upes.ac.in>
2025-11-24 23:18:29 +0530

    Add files via upload

---
+++ q81.c
@@ -0,0 +1,21 @@
+/*  Count characters in a string without using built-in length functions.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);  
+
+    while (str[count] != '\0')
+    { 
+        count++;    
+    }
+
+    printf("Length of string = %d\n", count);
+    return 0;
+}
